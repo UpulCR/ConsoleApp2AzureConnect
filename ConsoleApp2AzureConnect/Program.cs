87c62ff51c5d1717edce98ed1b2b94077eeb4274
UpulK <UpulK@CZC4521SQD>
2019-10-16 12:39:49 +0100

    Add project files.

---
+++ ConsoleApp2AzureConnect/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ConsoleApp2AzureConnect
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            SqlCommand cnnCommand;
+            SqlDataReader cnnReader;
+            var connectionString = ConfigurationManager.ConnectionStrings["EPWebServices"].ConnectionString;
+            using (SqlConnection cnn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    Console.WriteLine("Connection Open ! ");
+                    cnn.Open();
+                    cnnCommand = new SqlCommand("SELECT *  FROM [dbo].[UserAnswers] ",cnn);
+                    cnnReader = cnnCommand.ExecuteReader();
+                    while (cnnReader.Read())
+                    {
+                        
+                        Console.WriteLine(cnnReader[1].ToString());
+                    }
+
+                  
+                    cnn.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Can not open connection ! ");
+                }
+            }
+
+        }
+    }
+}
